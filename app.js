8ec6b9780373e54bcd70281a62a7ab055e866c61
batoolalomari <batoolalomari2@gmail.com>
2020-09-10 20:09:48 +0300

    new branch for problem 2

--- app.js
+++ app.js
@@ -27,11 +27,13 @@ Test this function by hand in the console to get it working, and when you think 
 
 // Write your code here
 function multiply(a, b) { //eslint-disable-line
-
-}
-
-// Here is the test for multiply(); uncomment it to run it
-// testMultiply(5,9);
+    var mul=a*b;
+    var str='The product of 5 and 9 is 45.';
+     return [mul,str];
+ }
+ multiply(5,9);
+ // Here is the test for multiply(); uncomment it to run it
+ testMultiply(5,9);
 
 // Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. Don't forget to create a new branch for your work on the next question!
 
