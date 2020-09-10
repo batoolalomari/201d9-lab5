17302d748754772142ebd44644ab7ca58fef1415
batoolalomari <batoolalomari2@gmail.com>
2020-09-10 20:49:22 +0300

    nothing changes , just to sure that nothink work wrong

--- app.js
+++ app.js
@@ -128,7 +128,10 @@ testMultiplyArray(testArray);
 
 /////////////////////////////////////
 /* STRETCH GOAL: Problem 6
-Write a function called multiplyAnyArray() that takes an array of numbers of any length as its argument and returns an array whose first element is the product of those numbers, and the second element is a string that EXACTLY follows this example and concatenates a message using the arguments that were passed into the function:
+Write a function called multiplyAnyArray() that takes an array of numbers of any length as its argument and
+ returns an array whose first element is the product of those numbers, and the second element is a string 
+ that EXACTLY follows this example and concatenates a message using the arguments that were passed into the 
+ function:
 "The numbers 1,2,3,4,5 have a product of 120."
 IMPORTANT DETAIL: You may not use the arithmetic operator * in this function. To do multiplication, use your multiply() function that you've already created. You're going to have to be resourceful to figure out how to do this. However, you may continue to use the + operator for string concatenation.
 This function should be dynamic, accepting an array of any length.
@@ -138,10 +141,11 @@ Test this function by hand in the console to get it working, and when you think 
 var testDynamicArray = [1,2,3,4,5]; //eslint-disable-line
 
 function multiplyAnyArray(dynamicArray) { //eslint-disable-line
-
-}
-
-// Here is the test for multiplyArray(); uncomment it to run it
-// testMultiplyAnyArray(testDynamicArray);
+    
+ 
+ }
+ 
+ // Here is the test for multiplyArray(); uncomment it to run it
+ // testMultiplyAnyArray(testDynamicArray);
 
 // Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. You're done! Submit the link to the repo following the instructions in Canvas.
